80a16d7ad3cb45fa1ec88b22152ca2000a95746d
Alex <trainofbird@gmail.com>
2023-04-25 20:48:58 +0300

    Решил первую задачу

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Main();
+
+void Main()
+{
+    int num1 = GiveRandNumber(); // Создаём первое случайное число
+    int num2 = GiveRandNumber(); // Создаём второе случайное число
+    Console.WriteLine("1-е число: " + num1);
+    Console.WriteLine("2-е число: " + num2);
+    if (num1 > num2)
+    {
+        Console.WriteLine("Первое число больше второго");
+    }
+    else if (num2 > num1)
+    {
+        Console.WriteLine("Второе число больше первого");
+    }
+    else if (num1 == num2)
+    {
+        Console.WriteLine("Числа равны");
+    }
+}
+
+int GiveRandNumber()
+{
+    int rand = new Random().Next(0, 100);
+    // Console.WriteLine(rand);
+    return rand;
+}
