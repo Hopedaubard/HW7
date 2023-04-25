1efea28431d6a206361e7e4b3927dc87fe374680
Alex <trainofbird@gmail.com>
2023-04-25 23:18:29 +0300

    Третья задача решена

---
+++ Task6/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Main();
+
+void Main()
+{
+    int num = GetRandNumber();
+    Console.WriteLine(num);
+    if (num % 2 == 0)
+    {
+        Console.WriteLine("Число чётное");
+    }
+    else
+    {
+        Console.WriteLine("Число нечётное");
+    }
+}
+
+int GetRandNumber()
+{
+    int rand = new Random().Next(-100, 101);
+    return rand;
+}
