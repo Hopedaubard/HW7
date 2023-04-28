6f2e9e4c19d84663a1eb86a86a689b40fe519198
Alex <trainofbird@gmail.com>
2023-04-29 00:37:19 +0300

    решено домашнее задание от семинара 2

---
+++ Task13/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Main();
+
+void Main()
+{
+    int number = Promt("Введите чсло, входящее в диапазон типа int: ");
+    // третьей цифры нет в том случае, если число двухзначное
+    if (number < 100 && number > -100)
+    {
+        Console.WriteLine("Число двухзначное, в нём нет третьей цифры");
+    }
+    else
+    {
+        FindThirdDigit(number);
+    }
+}
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return Math.Abs(result);
+}
+
+int FindThirdDigit(int num)
+{
+    {
+        while (num > 1000)
+        {
+            num = num / 10;
+        }
+        num = num % 10;
+        Console.WriteLine("Третья цифра введённого числа: " + num);
+    }
+    return num;
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Main();
+
+void Main()
+{
+    int weekDayNum = Promt("Введите число, обозначающее день недели: ");
+    bool checkDay = ValideWeekDay(weekDayNum);
+    if (checkDay == true)
+    {
+        CheckWeekEnd(weekDayNum);
+    }
+    else
+    {
+        Console.WriteLine("Это не день недели");
+    }
+}
+
+bool ValideWeekDay(int num)
+{
+
+    if (num > 0 && num <= 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+void CheckWeekEnd(int weekDay)
+{
+    if (weekDay == 6 || weekDay == 7)
+    {
+        Console.WriteLine("Отдыхай, выходной!");
+    }
+    else
+        Console.WriteLine("Работаем, скоро выходные!");
+}
